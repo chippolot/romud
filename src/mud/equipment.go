16241cf2f52165d1f95d1ecc7e82d13a5bfb5b10
Ben Smith <chippolot+github@gmail.com>
2023-01-16 06:27:55 -0800

    Missed a file

--- src/mud/equipment.go
+++ src/mud/equipment.go
@@ -233,86 +233,86 @@ func sendEquipMessages(e *Entity, w *World, slot EquipSlot, item *Item) {
 	if item.cfg.Equipment.Slot.Has(EqSlot_2H) {
 		switch slot {
 		case EqSlot_FingerL, EqSlot_FingerR, EqSlot_Fingers:
-			SendToPlayer(e, "You slide %s onto both of your ring fingrs", item.Name())
-			BroadcastToRoomRe(w, e, SendRst_None, "%s slides %s onto both of %s ring fingrs", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+			SendToPlayer(e, "You slide %s onto both of your ring fingrs", ObservableName(item))
+			BroadcastToRoomRe(w, e, SendRst_None, "%s slides %s onto both of %s ring fingrs", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 			return
 		case EqSlot_WristL, EqSlot_WristR, EqSlot_Wrists:
-			SendToPlayer(e, "You put %s around both of your wrists", item.Name())
-			BroadcastToRoomRe(w, e, SendRst_None, "%s puts %s onto both of %s wrists", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+			SendToPlayer(e, "You put %s around both of your wrists", ObservableName(item))
+			BroadcastToRoomRe(w, e, SendRst_None, "%s puts %s onto both of %s wrists", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 			return
 		case EqSlot_HeldL, EqSlot_HeldR, EqSlot_Held1H, EqSlot_Held2H:
-			SendToPlayer(e, "You grasp %s in both hands", item.Name())
-			BroadcastToRoomRe(w, e, SendRst_None, "%s grasps %s in both of %s hands", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+			SendToPlayer(e, "You grasp %s in both hands", ObservableName(item))
+			BroadcastToRoomRe(w, e, SendRst_None, "%s grasps %s in both of %s hands", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 			return
 		}
 	}
 	switch slot {
 	case EqSlot_Waist:
-		SendToPlayer(e, "You fasten %s around your waist", item.Name())
-		BroadcastToRoomRe(w, e, SendRst_None, "%s fastens %s around %s waist", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+		SendToPlayer(e, "You fasten %s around your waist", ObservableName(item))
+		BroadcastToRoomRe(w, e, SendRst_None, "%s fastens %s around %s waist", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 		return
 	case EqSlot_Neck1, EqSlot_Neck2, EqSlot_Neck:
-		SendToPlayer(e, "You wear %s around your neck", item.Name())
-		BroadcastToRoomRe(w, e, SendRst_None, "%s wears %s around %s neck", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+		SendToPlayer(e, "You wear %s around your neck", ObservableName(item))
+		BroadcastToRoomRe(w, e, SendRst_None, "%s wears %s around %s neck", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 		return
 	case EqSlot_Head:
-		SendToPlayer(e, "You wear %s on your head", item.Name())
-		BroadcastToRoomRe(w, e, SendRst_None, "%s wears %s on %s head", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+		SendToPlayer(e, "You wear %s on your head", ObservableName(item))
+		BroadcastToRoomRe(w, e, SendRst_None, "%s wears %s on %s head", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 		return
 	case EqSlot_Body:
-		SendToPlayer(e, "You wear %s around your body", item.Name())
-		BroadcastToRoomRe(w, e, SendRst_None, "%s wears %s around %s body", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+		SendToPlayer(e, "You wear %s around your body", ObservableName(item))
+		BroadcastToRoomRe(w, e, SendRst_None, "%s wears %s around %s body", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 		return
 	case EqSlot_Hands:
-		SendToPlayer(e, "You put %s on your hands", item.Name())
-		BroadcastToRoomRe(w, e, SendRst_None, "%s puts %s on %s hands", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+		SendToPlayer(e, "You put %s on your hands", ObservableName(item))
+		BroadcastToRoomRe(w, e, SendRst_None, "%s puts %s on %s hands", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 		return
 	case EqSlot_Arms:
-		SendToPlayer(e, "You wear %s on your arms", item.Name())
-		BroadcastToRoomRe(w, e, SendRst_None, "%s wears %s on %s arms", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+		SendToPlayer(e, "You wear %s on your arms", ObservableName(item))
+		BroadcastToRoomRe(w, e, SendRst_None, "%s wears %s on %s arms", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 		return
 	case EqSlot_Legs:
-		SendToPlayer(e, "You wear %s on your legs", item.Name())
-		BroadcastToRoomRe(w, e, SendRst_None, "%s wears %s on %s legs", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+		SendToPlayer(e, "You wear %s on your legs", ObservableName(item))
+		BroadcastToRoomRe(w, e, SendRst_None, "%s wears %s on %s legs", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 		return
 	case EqSlot_Feet:
-		SendToPlayer(e, "You wear %s on your feet", item.Name())
-		BroadcastToRoomRe(w, e, SendRst_None, "%s wears %s on %s feet", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+		SendToPlayer(e, "You wear %s on your feet", ObservableName(item))
+		BroadcastToRoomRe(w, e, SendRst_None, "%s wears %s on %s feet", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 		return
 	case EqSlot_FingerL:
-		SendToPlayer(e, "You slide %s on your left ring finger", item.Name())
-		BroadcastToRoomRe(w, e, SendRst_None, "%s slides %s on %s left ring finger", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+		SendToPlayer(e, "You slide %s on your left ring finger", ObservableName(item))
+		BroadcastToRoomRe(w, e, SendRst_None, "%s slides %s on %s left ring finger", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 		return
 	case EqSlot_FingerR:
-		SendToPlayer(e, "You slide %s on your right ring finger", item.Name())
-		BroadcastToRoomRe(w, e, SendRst_None, "%s slides %s on %s right ring finger", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+		SendToPlayer(e, "You slide %s on your right ring finger", ObservableName(item))
+		BroadcastToRoomRe(w, e, SendRst_None, "%s slides %s on %s right ring finger", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 		return
 	case EqSlot_WristL:
-		SendToPlayer(e, "You put %s around your left wrist", item.Name())
-		BroadcastToRoomRe(w, e, SendRst_None, "%s puts %s around %s left wrist", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+		SendToPlayer(e, "You put %s around your left wrist", ObservableName(item))
+		BroadcastToRoomRe(w, e, SendRst_None, "%s puts %s around %s left wrist", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 		return
 	case EqSlot_WristR:
-		SendToPlayer(e, "You put %s around your right wrist", item.Name())
-		BroadcastToRoomRe(w, e, SendRst_None, "%s puts %s around %s right wrist", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+		SendToPlayer(e, "You put %s around your right wrist", ObservableName(item))
+		BroadcastToRoomRe(w, e, SendRst_None, "%s puts %s around %s right wrist", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 		return
 	case EqSlot_Shoulders:
-		SendToPlayer(e, "You wear %s over your shoulders", item.Name())
-		BroadcastToRoomRe(w, e, SendRst_None, "%s wears %s over %s shoulders", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+		SendToPlayer(e, "You wear %s over your shoulders", ObservableName(item))
+		BroadcastToRoomRe(w, e, SendRst_None, "%s wears %s over %s shoulders", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 		return
 	case EqSlot_HeldL:
-		SendToPlayer(e, "You grasp %s in your left hand", item.Name())
-		BroadcastToRoomRe(w, e, SendRst_None, "%s grasps %s in %s left hand", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+		SendToPlayer(e, "You grasp %s in your left hand", ObservableName(item))
+		BroadcastToRoomRe(w, e, SendRst_None, "%s grasps %s in %s left hand", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 		return
 	case EqSlot_HeldR:
-		SendToPlayer(e, "You grasp %s in your right hand", item.Name())
-		BroadcastToRoomRe(w, e, SendRst_None, "%s grasps %s in %s right hand", e.NameCapitalized(), item.Name(), e.Gender().GetPossessivePronoun())
+		SendToPlayer(e, "You grasp %s in your right hand", ObservableName(item))
+		BroadcastToRoomRe(w, e, SendRst_None, "%s grasps %s in %s right hand", ObservableNameCap(e), ObservableName(item), e.Gender().GetPossessivePronoun())
 		return
 	}
 }
 
 func sendUnequipMessages(e *Entity, w *World, item *Item) {
-	SendToPlayer(e, "You unequip %s", item.Name())
-	BroadcastToRoomRe(w, e, SendRst_None, "%s unequip %s", e.NameCapitalized(), item.Name())
+	SendToPlayer(e, "You unequip %s", ObservableName(item))
+	BroadcastToRoomRe(w, e, SendRst_None, "%s unequip %s", ObservableNameCap(e), ObservableName(item))
 }
 
 type EquipmentConfig struct {

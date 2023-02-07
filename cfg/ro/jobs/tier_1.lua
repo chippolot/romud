eb2cf71da51c02e88806477e1eed993af4044981
Ben Smith <chippolot@gmail.com>
2023-02-07 09:57:21 -0800

    Adding Knight and Priest

--- cfg/ro/jobs/tier_1.lua
+++ cfg/ro/jobs/tier_1.lua
@@ -1,36 +1,6 @@
 require('cfg.ro.utils.go-api')
 
 Config.NewJob({
-    JobType = JobType.Novice,
-    JobTier = JobTier.Novice,
-    Name = "Novice",
-    Skills = {
-        "first_aid",
-        "play_dead"
-    },
-    AspdBase = {
-        [WeaponType.Fist] = 40,
-        [WeaponType.Dagger] = 55,
-        [WeaponType.Sword_1H] = 57,
-        [WeaponType.Axe_1H] = 50,
-        [WeaponType.Mace_1H] = 50,
-        [WeaponType.Mace_2H] = 55,
-        [WeaponType.Staff_1H] = 65,
-        [WeaponType.Staff_2H] = 65,
-    },
-    MaxHPMod = 0,
-    MaxSPMod = 1.0,
-    CarryingCapacityMod = 0,
-    BonusStats = {
-        { Level = 2, Stat = Stat.Luk },
-        { Level = 3, Stat = Stat.Dex },
-        { Level = 5, Stat = Stat.Agi },
-        { Level = 6, Stat = Stat.Vit },
-        { Level = 8, Stat = Stat.Str },
-        { Level = 9, Stat = Stat.Int },
-    }
-})
-Config.NewJob({
     JobType = JobType.Swordman,
     JobTier = JobTier.First,
     Base = JobType.Novice,

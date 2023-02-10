5be38138c3befe3d61db1e0f5d7581b67faf3dea
Ben Smith <chippolot@gmail.com>
2023-02-10 10:14:09 -0800

    Adding honey item

--- cfg/ro/items/healing.lua
+++ cfg/ro/items/healing.lua
@@ -1,9 +1,22 @@
 require('cfg.ro.utils.go-api')
 
-local function UseEdibleHealingItem(user, item, min, max) 
-    local amt = Util.RandomRange(min, max)
-    Entity.HealHP(user, amt)
-    Write.ToPlayer(user, string.format("You eat %s <c green>(%d)</c>", Item.Name(item), amt))
+local function UseEdibleHealingItem(user, item, minHP, maxHP, minSP, maxSP) 
+
+    local hpStr = ""
+    local amtHP = Util.RandomRange(minHP, maxHP)
+    if amtHP ~= 0 then
+        Entity.HealHP(user, amtHP)
+        hpStr = string.format(" <c green>(%dhp)</c>", amtHP)
+    end
+
+    local spStr = ""
+    local amtSP = Util.RandomRange(minSP, maxSP)
+    if amtSP ~= 0 then
+        Entity.HealSP(user, amtSP)
+        spStr = string.format(" <c green>(%dsp)</c>", amtSP)
+    end
+
+    Write.ToPlayer(user, string.format("You eat %s%s%s", Item.Name(item), hpStr,spStr))
     Write.ToRoom(Entity.Room(user), {user}, string.format("%s eats %s.", Entity.NameCap(user), Item.Name(item)))
     return true
 end
@@ -26,7 +39,7 @@ Config.NewItem({
     },
     Scripts = {
         Use = function(user, item) 
-            return UseEdibleHealingItem(user, item, 16, 22)
+            return UseEdibleHealingItem(user, item, 16, 22, 0, 0)
         end
     }
 })
@@ -38,7 +51,7 @@ Config.NewItem({
 	Name = "a red herb",
 	NamePlural = "red herbs",
 	RoomDesc = "Some sprigs of red herb are here.",
-    FullDesc = "	A weak medicinal herb which heals wounds. Recovers a little HP.",
+    FullDesc = "A weak medicinal herb which heals wounds. Recovers a little HP.",
 	BuyPrice = 18,
     SellPrice = 9,
 	Weight = 3,
@@ -47,7 +60,28 @@ Config.NewItem({
     },
     Scripts = {
         Use = function(user, item) 
-            return UseEdibleHealingItem(user, item, 18, 28)
+            return UseEdibleHealingItem(user, item, 18, 28, 0, 0)
+        end
+    }
+})
+Config.NewItem({
+    Key = "honey",
+	Keywords = {
+        "honey", "pot"
+	},
+	Name = "a small pot of honey",
+	NamePlural = "small pots of honey",
+	RoomDesc = "A small pot of honey is here.",
+    FullDesc = "A sweet product made by bees that is endowed with yummy flavor and medicinal uses.",
+	BuyPrice = 500,
+    SellPrice = 250,
+	Weight = 10,
+    Flags = {
+        "usable", "consumable"
+    },
+    Scripts = {
+        Use = function(user, item) 
+            return UseEdibleHealingItem(user, item, 70, 100, 20, 40)
         end
     }
 })
